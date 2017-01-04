c3109807b3d2544644516e51b65d93aa580b73de
Travis Walentin <travis.walentin@gmail.com>
2017-01-03 17:59:33 -0800

    complete to spec, need to work on bonus

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+var db = require('./models');
+var Hashids = require("hashids");
+var hashids = new Hashids("The 256-bit NaCl for the hash!");
 
+//middleware setup
+app.set('view engine', 'ejs');
 app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(ejsLayouts);
+
 
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  res.render('home');
 });
 
-var server = app.listen(process.env.PORT || 3000);
+app.post('/links', function(req,res){
+  db.link.create(req.body).then(function(link){
+    res.redirect('/links/'+link.id);
+  });
+});
+
+app.get('/links/:id', function(req,res){
+  db.link.find({
+    where: {id: req.params.id},
+  })
+  .then(function(link){
+    var hash = hashids.encode(link.id);
+    console.log("Hash is "+hash);
+    res.render('links', {hash:hash});
+  });
+});
+
+app.get('/:hash', function(req,res){
+  console.log(req.params.hash);
+  var hash = hashids.decode(req.params.hash);
+  console.log("decoded has is "+hash);
+  db.link.find({
+    where: {id: hash},
+  })
+  .then(function(link){
+    res.redirect(link.url);
+  });
+});
+
+
+var server = app.listen(process.env.PORT || 3001);
 
 module.exports = server;
